0df2c2792eaef53553ae28a2ece170f427738e3e
kokone164 <degawakokone@outlook.jp>
2026-07-11 08:49:28 +0900

    ファイル作成

--- 20260627_TaskGame_degawakokone/20260627_TaskGame_degawakokone/Main.cpp
+++ 20260627_TaskGame_degawakokone/20260627_TaskGame_degawakokone/Main.cpp
@@ -1,2 +1,49 @@
-#include<iostream>
-using namespace std;
+#include"DxLib.h"
+#include"Game.h"
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+	ChangeWindowMode(TRUE);
+	DxLib_Init();
+	SetGraphMode(800, 600, 32);
+	SetDrawScreen(DX_SCREEN_BACK);
+	if (DxLib_Init() == -1) return -1;
+
+	//インスタンス
+	Game game;
+
+	int prevTime = GetNowCount();
+	int prevMouse = 0;
+
+	while (ProcessMessage() == 0 &&
+		CheckHitKey(KEY_INPUT_ESCAPE) == 0)
+	{
+		int nowTimer = GetNowCount();
+		float dt = (nowTimer - prevTime) / 1000.0f;
+		prevTime = nowTimer;
+		int mx, my;
+		GetMousePoint(&mx, &my);
+		int mouse = GetMouseInput();
+		bool click = false;
+
+		if ((mouse & MOUSE_INPUT_LEFT) && !(prevMouse & MOUSE_INPUT_LEFT))
+		{
+			click = true;
+		}
+
+		prevMouse = mouse;
+		//更新
+		game.Update(dt, mx, my, click);
+
+		ClearDrawScreen();
+
+		//描画
+		game.Draw();
+
+		ScreenFlip();
+	}
+
+	DxLib_End();
+
+	return 0;
+}
